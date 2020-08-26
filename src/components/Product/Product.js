73a73cab48e8f8eab3eb082b08d86d8ac80162f0
as ahmed <rakibasrr13@gmail.com>
2020-08-26 21:58:10 +0300

    50% done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee,faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
-  console.log(props.product);
+  console.log(props);
   const { img, name, price, seller, stock } = props.product;
   return (
     <div className="product">
@@ -13,6 +15,10 @@ const Product = (props) => {
         <p><small>{seller}</small></p>
         <h3>${price}</h3>
         <p><small>only {stock} left in stock - Order soon</small></p>
+        <button
+         className="main-btn"
+        onClick={() => props.handleAddProduct(props.product)}
+         ><span className="cart-icon"><FontAwesomeIcon icon={faShoppingCart} /></span>add to cart</button>
       </div>
 
     </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,15 +3,21 @@ import './Shop.css';
 import fakeData from '../../fakeData'
 import Product from '../Product/Product';
 const Shop = () => { 
-    const first10 = fakeData.slice(0,10);
+     const first10 = fakeData.slice(0,10);
      const [products,setProducts] = useState(first10)
- 
+     const handleAddProduct = (product) =>{
+         console.log('Rakib',product)
+     }
+     //       onClick={() => props.handleAddProduct(props.product)}
     return (
 
         <div className="shop-container">
             <div className="product-container">
                  {
-                     products.map(pd => <Product product={pd}></Product>)
+                     products.map(pd => 
+                     <Product 
+                     product={pd} handleAddProduct={handleAddProduct}
+                     ></Product>)
                  }
             </div>
             <div className="cart-container">
