a0f321f7f8db496c4cd79e6844973f4408ac120a
as ahmed <rakibasrr13@gmail.com>
2020-08-27 11:50:37 +0300

    Simple ema-john is done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const total = cart.reduce((total,prd)=> total + prd.price,0)
+
+    let shipping = 0;
+     if(total > 35){
+
+        shipping = 0;
+
+    }
+     else if(total>15){
+
+        shipping = 4.99;
+
+    }
+    else if(total > 0){
+        shipping = 12.99;
+    }
+
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = total + shipping + Number(tax).toFixed(2);
+    const formatNumber = num =>{
+    const precision = num.toFixed(2);
+    return Number(precision);
+ }
+
+    return (
+        <div>
+                <h4>Order Summery</h4>
+                <p>items Ordered:{props.cart.length}</p>
+                <p>product Price:{formatNumber(total)}</p>
+                <p>Shipping Const:{shipping}</p>
+                <p>tax + vat:{tax}</p>
+                <p>Total Price:{grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,28 +2,32 @@ import React, { useState } from 'react';
 import './Shop.css';
 import fakeData from '../../fakeData'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 const Shop = () => { 
      const first10 = fakeData.slice(0,10);
      const [products,setProducts] = useState(first10)
+
+     const [cart,setCart] = useState([])
+
      const handleAddProduct = (product) =>{
+         const newCart = [...cart,product]
+         setCart(newCart)
          console.log('Rakib',product)
      }
-     //       onClick={() => props.handleAddProduct(props.product)}
-    return (
+
+     return (
 
         <div className="shop-container">
             <div className="product-container">
                  {
                      products.map(pd => 
                      <Product 
-                     product={pd} handleAddProduct={handleAddProduct}
-                     ></Product>)
+                      product={pd} handleAddProduct={handleAddProduct}
+                    ></Product>)
                  }
             </div>
             <div className="cart-container">
-                <h1>Rakib</h1>
-              
-              
+                <Cart cart={cart}></Cart>
             </div>
         </div>
         
