97ba352574f97d277b8d8129bf47b0c672e1636d
as ahmed <rakibasrr13@gmail.com>
2020-09-16 11:28:07 +0300

    ema-john-simple

--- src/components/Login/LoginManager.js
+++ src/components/Login/LoginManager.js
@@ -1,6 +1,6 @@
 import *as firebase from 'firebase/app';
 import "firebase/auth";
-import firebaseConfig from '../firebase.config';
+import firebaseConfig from '../../firebase.config';
 
     export const initializeLoginFramework = () => {
     if (firebase.apps.length === 0) {
